76646e775856d83b6f134b94f60b5377cca15274
Damian Lewis <damian@damianlewis.co.uk>
2024-08-27 20:10:36 +0100

    feat: add key name environment variable

--- ssh.sh
+++ ssh.sh
@@ -7,19 +7,21 @@ if [[ -z "$1" ]]; then
     exit 1
 fi
 
+KEY_NAME="id_ed25519_github"
+
 # Generating a new SSH key
 # https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#generating-a-new-ssh-key
-ssh-keygen -t ed25519 -C "$1" -f ~/.ssh/id_ed25519
+ssh-keygen -t ed25519 -C "$1" -f "$HOME/.ssh/$KEY_NAME"
 
 # Adding your SSH key to the ssh-agent
 # https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#adding-your-ssh-key-to-the-ssh-agent
 eval "$(ssh-agent -s)"
 
 touch ~/.ssh/config
-echo "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_ed25519" | tee ~/.ssh/config
+echo "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/$KEY_NAME" | tee ~/.ssh/config
 
-ssh-add -K ~/.ssh/id_ed25519
+ssh-add -K "$HOME/.ssh/$KEY_NAME"
 
 # Adding your SSH key to your GitHub account
 # https://docs.github.com/en/github/authenticating-to-github/adding-a-new-ssh-key-to-your-github-account
-echo "run 'pbcopy < ~/.ssh/id_ed25519.pub' and paste that into GitHub"
+echo "run 'pbcopy < ~/.ssh/$KEY_NAME.pub' and paste that into GitHub"
